2ea8546e154cfe4cb64348609efcdfe4c223d116
ahmadgholiyan <84031570+ahmadgholiyan@users.noreply.github.com>
2023-04-16 18:51:32 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 var http = require('http');
+const express = require("express");
+const app = express();
+var exec = require("child_process").exec;
+const os = require("os");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+var request = require("request");
+var fs = require("fs");
+var path = require("path");
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
+//下载web可执行文件
+app.get("/download", (req, res) => {
+  download_web((err) => {
+    if (err) res.send("下载文件失败");
+    else res.send("下载文件成功");
+  });
+});
+
+app.use(
+  "/",
+  createProxyMiddleware({
+    target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
+    changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
+    ws: true, // 是否代理websockets
+    pathRewrite: {
+      // 请求中去除/
+      "^/": "/",
+    },
+    onProxyReq: function onProxyReq(proxyReq, req, res) { },
+  })
+);
+//启动web
+app.get("/start", (req, res) => {
+  let cmdStr =
+    "chmod +x ./web.js && ./web.js -c ./config.json >/dev/null 2>&1 &";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.send("命令行执行错误：" + err);
+    } else {
+      res.send("命令行执行结果：" + "启动成功!");
+    }
+  });
+});
